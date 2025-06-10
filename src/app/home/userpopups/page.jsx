37bb5e4be1a1b5aaa34210d8f634c206b21945cb
AKAGEGE87 <akagege87@gmail.com>
2025-06-10 16:58:30 +0400

    1

---
+++ src/app/home/userpopups/page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+  return <div>Hom Page User Popups</div>;
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="bg-[#1B6392] w-[100%] py-[16px] px-[300px] text-center">
+      Header
+    </div>
+  );
+}
